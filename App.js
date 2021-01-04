1072f83f6d901ab276d85df59a890955e651a252
MaxKichiy <6aJIa6oJI1336@gmail.com>
2021-01-04 05:51:27 +0200

    add a FlatList

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
 
 export default function App() {
   const [goal, setGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setGoal(enteredText);
+  };
+
+  const confirmHandler = () => {
+    setCourseGoals((prevState) => [...prevState, { key: Math.random().toString(), value: goal }]);
+  };
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Enter your goal' style={styles.textInput} />
-        <Button title='ADD' style={styles.button} />
+        <TextInput
+          placeholder='Enter your goal'
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={goal}
+        />
+        <Button title='ADD' style={styles.button} onPress={confirmHandler} />
       </View>
+      <FlatList
+        data={courseGoals}
+        renderItem={(goal) => (
+          <View style={styles.listItem}>
+            <Text>{goal.item.value}</Text>
+          </View>
+        )}></FlatList>
     </View>
   );
 }
@@ -16,10 +37,11 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50,
-    flexDirection: 'row',
-    alignItems: 'flex-start',
+    alignItems: 'center',
     backgroundColor: '#aaa',
     height: '100%',
+
+    flexWrap: 'wrap',
   },
   textInput: {
     width: '80%',
@@ -29,12 +51,16 @@ const styles = StyleSheet.create({
     borderWidth: 2,
   },
   inputContainer: {
+    // width: '100%',
     flexDirection: 'row',
-    flexGrow: 1,
-    justifyContent: 'space-between',
     alignItems: 'center',
   },
-  button: {
-    color: 'green',
+  listItem: {
+    width: 200,
+    marginVertical: 10,
+    padding: 10,
+    backgroundColor: '#cdcdcd',
+    borderColor: 'black',
+    borderWidth: 1,
   },
 });
